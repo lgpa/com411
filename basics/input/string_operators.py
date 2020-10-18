2539516dc5366ddde1adf3fa54048a9be4f3c6c7
repl.it user <replituser@example.com>
2020-10-18 17:05:16 +0000

    string operators

---
+++ basics/input/string_operators.py
@@ -0,0 +1,12 @@
+print("Please enter number of lives")
+lives = int(input())
+
+print("Please enter energy level")
+energy = int(input())
+
+print("Please enter shield level")
+shield = int(input()) 
+
+print("Lives:", "♥" * lives)
+print("Energy:", "♦" * energy)
+print("Shield:", "♦" * shield)
