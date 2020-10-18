b882b473e584e3367b78faf9c85b1cd58aed77a6
repl.it user <replituser@example.com>
2020-10-18 17:23:09 +0000

    elif

---
+++ basics/decisions/simple_decisions/if_elif_else.py
@@ -0,0 +1,12 @@
+print("Towards which direction should I paint (up, down, left or right)?")
+direction = str(input())
+if(direction == "up"):
+  print("I am painting in the upward direction!")
+elif(direction == "down"):
+  print("I am painting in the downward direction!")
+elif(direction == "left"):
+  print("I am painting in the left direction!")
+elif(direction == "right"):
+  print("I am painting in the right direction!")
+else:
+  print("Im not painting")
---
+++ basics/decisions/simple_decisions/if_else.py
@@ -0,0 +1,7 @@
+print("Please enter the activity to be performed.")
+act = str(input())
+if(act == "calculate"):
+  print("Performing calculations...")
+else:
+  print("Performing activity...")
+print("\n Activity completed!")
